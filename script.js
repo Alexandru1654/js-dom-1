4cea42a8da249eb8ef3483d83b30764f449e1547
alexandru233847 <alexutualexino33@gmail.com>
2024-10-08 18:52:34 +0200

    Bug adjust

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const lampOFF = document.getElementById('lamp-off')
+const lampOFF = document.getElementById('lamp-off');
+const lampOn = document.getElementById('lamp-on');
+const lampON = document.getElementById('turn-on');
+const lampOff = document.getElementById('turn-off');
 
-const lampOn = document.getElementById('lamp-on')
+// Power button click event
+lampOn.addEventListener('click', function () {
+    lampON.style.display = 'block';
+    lampOff.style.display = 'none';
+});
 
-const lampON = document.getElementById('turn-on')
-
-const lampOff = document.getElementById('turn-off')
+// Power off button click event
+lampOFF.addEventListener('click', function () {
+    lampOff.style.display = 'block';
+    lampON.style.display = 'none';
+});
 
