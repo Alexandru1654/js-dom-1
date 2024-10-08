e1aa551de0a1475d753146fe688e78b7f347b3ba
alexandru233847 <alexutualexino33@gmail.com>
2024-10-08 17:24:26 +0200

    Function

---
+++ script.js
@@ -0,0 +1,8 @@
+const lampOFF = document.getElementById('lamp-off')
+
+const lampOn = document.getElementById('lamp-on')
+
+const lampON = document.getElementById('turn-on')
+
+const lampOff = document.getElementById('turn-off')
+
